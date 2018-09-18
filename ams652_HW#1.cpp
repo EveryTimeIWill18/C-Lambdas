db981cfe32738a0f0f9a50a3b2ef169492e6797b
William Murphy <30298478+EveryTimeIWill18@users.noreply.github.com>
2018-09-18 08:59:39 -0400

    ams562 hw#1 final
    final code

--- ams652_HW#1.cpp
+++ ams652_HW#1.cpp
@@ -3,27 +3,38 @@
 #include <vector>
 #include <algorithm>
 
+#define STATEMENT "Please enter your "
 
 int main(){
 
-  std::string title, first_name, last_name, DOB, Bio;
+  std::string title, first_name, last_name, dob, bio;
+  std::vector<std::string> v = {title, first_name, last_name, dob, bio};
+  std::vector<std::string> w = {"title", "first name", "last name", "DOB", "Bio"};
+  std::vector<std::string> output;
+  int iter = 0;
+  int j    = 0;
 
-  std::vector<std::string> v = {"title", "first name", "last name", "DOB", "Bio"};
-  std::vector<std::string> input = {title, first_name, last_name, DOB, Bio};
-  std::for_each(v.begin(), v.end(),
-  [&input](std::string s){
-    int i = 0;
-    std::cout <<"Please enter your " << s << ": "<< std::endl;
-    std::cin >> input[i];
-
+  std::for_each(w.begin(), w.end(),
+  [&v, &w, &output, &iter](std::string s){
+    std::cout << STATEMENT << s << ": " << std::endl;
+    std::getline(std::cin, v[iter]);
+    output.push_back(v[iter]);
+    iter += 1;
   });
-  
 
-  //std::cout << "Please enter your title: ";
-  //std::cin >>  title;
-
-
-
+  std::for_each(output.begin(), output.end(),
+  [&j](std::string s){
+    if(j < 3){
+      std::cout << s <<" ";
+    }
+    if(j == 3){
+      std::cout << "\n" <<"\t" <<"DOB: " << s << std::endl;
+    }
+    if(j == 4){
+      std::cout << "\t" << "Bio: " << s << std::endl;
+    }
+    j += 1;
+  });
 
   return 0;
 }
